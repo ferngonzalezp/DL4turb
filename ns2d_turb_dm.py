5270541f4de2682f451860062988427a228e3868
Fernando Adán González <58112669+ferngonzalezp@users.noreply.github.com>
2022-12-02 11:55:21 +0100

    Add files via upload

--- ns2d_turb_dm.py
+++ ns2d_turb_dm.py
@@ -19,10 +19,11 @@ class mydataset(torch.utils.data.Dataset):
       
 class ns2d_dm(pl.LightningDataModule):
   
-  def __init__(self,path,batch_size):
+  def __init__(self,path,batch_size, test_all=False):
     super().__init__()
     self.path = path
     self.batch_size = batch_size
+    self.test_all = test_all
     
   def prepare_data(self):
     return None
@@ -30,6 +31,10 @@ class ns2d_dm(pl.LightningDataModule):
     data = xarray.open_mfdataset(self.path+'/*.nc', engine='netcdf4', concat_dim='batch', combine='nested', parallel=True)
     N = len(data['batch'])
     self.train_data, self.val_data = [mydataset(data.isel(batch=slice(0,N*9//10))),  mydataset(data.isel(batch=slice(N*9//10,N)))]
+    if self.test_all:
+      self.test_data = mydataset(data)
+    else:
+      self.test_data = self.val_data
 
   def train_dataloader(self):
     return DataLoader(self.train_data, batch_size=self.batch_size, num_workers=0, shuffle=True)
@@ -38,4 +43,4 @@ class ns2d_dm(pl.LightningDataModule):
     return DataLoader(self.val_data, batch_size=self.batch_size, num_workers=0)
 
   def test_dataloader(self):
-    return DataLoader(self.val_data, batch_size=self.batch_size, num_workers=0)
+    return DataLoader(self.test_data, batch_size=self.batch_size, num_workers=0)
